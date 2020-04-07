6c602e32c7c8695ebdde2b9497bd3bb2dc97a350
rishabh-ranjan <mr.rishabh.ranjan@gmail.com>
2020-04-07 15:57:46 +0530

    simple resolution done; testing remains

--- main.ml
+++ main.ml
@@ -7,32 +7,32 @@ let vars t =
     in IntSet.elements (aux t)
 
 let rec subst s = function
-| Var v -> (match StringMap.find_opt v s with | None -> Var v | Some t -> t)
+| Var v -> (match IntMap.find_opt v s with | None -> Var v | Some t -> t)
 | Node (x, l) -> Node (x, List.map (subst s) l)
 
 let compose s1 s2 =
-    StringMap.map (subst s2) s1 |> StringMap.union (fun k x y -> Some y) s2
+    IntMap.map (subst s2) s1 |> IntMap.union (fun k x y -> Some y) s2
 
 let rec occurs v = function
 | Var x when x = v -> true | Var _ -> false
 | Node (_, l) -> List.exists (occurs v) l
 
-exception Not_unifiable
-
-let iden_subst = StringMap.empty
+let iden_subst = IntMap.empty
 
 let rec mgu t1 t2 = match t1, t2 with
-| Var x, Var y when x = y -> iden_subst
-| Var x, t when (occurs x t) -> raise Not_unifiable
-| t, Var x when (occurs x t) -> raise Not_unifiable
-| Var x, t | t, Var x -> StringMap.singleton x t
-| Node (f, _), Node (g, _) when f <> g -> raise Not_unifiable
+| Var x, Var y when x = y -> Some iden_subst
+| Var x, t when (occurs x t) -> None
+| t, Var x when (occurs x t) -> None
+| Var x, t | t, Var x -> Some (IntMap.singleton x t)
+| Node (f, _), Node (g, _) when f <> g -> None
 | Node (_, xl), Node (_, yl) ->
     let rec aux acc xl yl = match xl, yl with
-    | [], [] -> acc
-    | [], _ | _, [] -> raise Not_unifiable
-    | x::xs, y::ys ->
-        let m = mgu (subst acc x) (subst acc y) in aux (compose acc m) xs ys
+    | [], [] -> Some acc
+    | [], _ | _, [] -> None
+    | x::xs, y::ys -> (match mgu (subst acc x) (subst acc y) with
+        | None -> None
+        | Some m -> aux (compose acc m) xs ys
+    )
     in aux iden_subst xl yl
 
 let rec conv_term_list tab l =
@@ -54,6 +54,22 @@ let conv_clause (t, l) =
     (t', l')
 
 let conv_kb = List.map conv_clause
+
+let rec conv_goal_term_list tabs l =
+    let tabs, rl' = List.fold_left (fun (tabs, acc) t ->
+        let tabs, t' = conv_goal_term tabs t in tabs, (t'::acc)
+    ) (tabs, []) l in
+    tabs, List.rev rl'
+
+and conv_goal_term ((tab, inv) as tabs) = function
+| Var v -> (match StringMap.find_opt v tab with
+    | None -> let n = StringMap.cardinal tab in
+        (StringMap.add v n tab, IntMap.add n v inv), Var n
+    | Some n -> tabs, Var n
+) 
+| Node (x, l) -> let tabs, l' = conv_goal_term_list tabs l in tabs, Node (x, l')
+
+let conv_goal = conv_goal_term_list (StringMap.empty, IntMap.empty)
 
 let rec next_min (c, l) = match l with
 | [] -> c+1, []
@@ -82,15 +98,61 @@ let rename_clause ft (t, l) =
     let _, _, l' = rename_term_list nex tab l in
     (t', l')
 
+let rec resolve_term_list kb = List.fold_left (resolve_term kb)
+
+and resolve_term kb sub_opt tm = match sub_opt with
+| None -> None
+| Some sub ->
+    let tm = subst sub tm in
+    let rec aux = function
+    | [] -> None
+    | kh::kt -> 
+        let (t, l) = rename_clause tm kh in
+        (match mgu t tm with
+        | None -> aux kt
+        | Some m ->
+            (match resolve_term_list kb (Some m) l with
+            | None -> aux kt
+            | Some s -> Some (compose (compose sub m) s)
+            )
+        )
+    in aux kb
+
+let resolve_goal kb = resolve_term_list kb (Some iden_subst)
+
+let rec print_soln_term_list inv = function
+| [] -> ()
+| h::[] -> print_soln_term inv h
+| h::t -> print_soln_term inv h; print_string ", "
+
+and print_soln_term inv = function
+| Var v -> (match IntMap.find_opt v inv with
+    | None -> print_string "_"; print_int v
+    | Some s -> print_string s
+)
+| Node (x, l) ->
+    print_string x; print_string "("; print_soln_term_list inv l; print_string ")"
+
+let print_soln inv = IntMap.iter (fun k v ->
+    (match IntMap.find_opt k inv with
+    | None -> print_string "_"; print_int k
+    | Some s -> print_string s
+    ); print_string " = "; print_soln_term inv v; print_string ".\n"
+)
+
 let reconsult file_name =
     let in_channel = open_in file_name in
     let lexbuf = Lexing.from_channel in_channel in
     conv_kb (Parser.prog Lexer.scan lexbuf)
 
-let query () =
+let query kb =
     print_string "?- "; flush stdout;
     let lexbuf = Lexing.from_channel stdin in
-    Parser.goal Lexer.scan lexbuf
+    let (_, inv), g = Parser.goal Lexer.scan lexbuf |> conv_goal in
+    (match resolve_goal kb g with
+    | None -> print_string "\nfalse.\n"
+    | Some s -> print_soln inv s; print_string "\ntrue.\n"
+    ); flush stdout
 
 let rec print_term = function
 | Var v -> print_string "_"; print_int v
@@ -120,8 +182,5 @@ let parse_in parse_fn conv_fn =
 
 let parse_prog () = parse_in Parser.prog conv_kb
 let parse_clause () = parse_in Parser.clause conv_clause
-let parse_term () = parse_in Parser.term (conv_term StringMap.empty)
+let parse_term () = snd (parse_in Parser.term (conv_term StringMap.empty))
 
-let () =
-    print_kb (reconsult "prog/succmath_simple.pl");
-    flush stdout;
